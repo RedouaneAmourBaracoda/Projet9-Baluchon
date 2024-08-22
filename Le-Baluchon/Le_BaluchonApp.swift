b8a9a16cf424066e54100b7c3e4e8d62c66818ec
RedouaneAmourBaracoda <redouane.amour@baracoda.com>
2024-08-22 18:23:24 +0200

    initial iOS App

---
+++ Le-Baluchon/Le_BaluchonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Le_BaluchonApp.swift
+//  Le-Baluchon
+//
+//  Created by Redouane on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Le_BaluchonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
